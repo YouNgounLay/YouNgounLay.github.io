0bd6ab55e46623b0165bdbf6bc30b45fa14ea2d5
Simon Lay <ylay3633@uni.sydney.edu.au>
2024-06-21 16:43:31 +1000

    update webfinger

--- run.py
+++ run.py
@@ -2,12 +2,14 @@ import requests
 
 # url = "https://mastodon.social/users/pbandj9819"
 # url = "https://mastodon.social/@pbandj9819"
-url = "https://daniellay.cc/dlay"
+url = "https://youngounlay.github.io/.well-known/webfinger?resource=acct:ylay@youngounlay.github.io"
+url = 'https://mastodon.social/users/pbandj9819'
 
 response = requests.get(url)
 if response.ok:
     try:
-        print(response.json())
+        res = response.json()
+        print(type(res))
     except Exception:
         print("There is no json body")
         print("\n" * 10)
