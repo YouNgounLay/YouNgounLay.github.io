5889538cee0a3c927979e7e70272f38782b951fd
Simon Lay <ylay3633@uni.sydney.edu.au>
2024-06-24 10:47:21 +1000

    update webfinger structure

--- run.py
+++ run.py
@@ -2,13 +2,14 @@ import requests
 
 url = "https://daniellay.cc/.well-known/webfinger?resource=acct:dlay@daniellay.cc"
 url = "https://daniellay.cc/u/dlay"
+url = "http://localhost:1313/user-info/ylay/actor"
 headers = {"Accept": "application/json"}
 response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     # print(response.text)
-    # json_content = response.json()
-    # print(json_content)
-    print(response.content)
+    json_content = response.json()
+    print(json_content)
+    # print(response.content)
 else:
     print("Failed to retrieve the JSON content.")
