bdc5749ae5b6320e927bcdd0a0cbdd54195aa93b
Simon Lay <ylay3633@uni.sydney.edu.au>
2024-06-24 10:40:27 +1000

    update webfinger

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 import requests
 
-url = "https://mastodon.social/users/pbandj9819"
-url = "http://localhost:1313/users/ylay"
+url = "https://daniellay.cc/.well-known/webfinger?resource=acct:dlay@daniellay.cc"
+url = "https://daniellay.cc/u/dlay"
 headers = {"Accept": "application/json"}
 response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
-    print(response.text)
-    print(response.json())
+    # print(response.text)
     # json_content = response.json()
     # print(json_content)
+    print(response.content)
 else:
     print("Failed to retrieve the JSON content.")
--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 MSG=$1
 git commit -am "${MSG}"
 git push origin main
+
+#!/bin/bash
+# Turn off the Wi-Fi interface on macOS
+
+# Command to turn off Wi-Fi
+networksetup -setairportpower airport off
+
+# Notify the user that Wi-Fi has been turned off
+echo "Wi-Fi has been turned off."
