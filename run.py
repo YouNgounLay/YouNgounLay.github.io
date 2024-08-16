4b39fb6b868d92804ebb7e173fbda9dbad2652c0
Simon Lay <ylay3633@uni.sydney.edu.au>
2024-08-16 12:26:41 +1000

    restructure followers and following

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ import json
 protocol = "https://"
 domain = "mastodon.social/users/pbandj9819"
 domain = "mastodon.social/inbox"
-url = f"{protocol}{domain}"
+domain = "gopinath.org/actors/rahul"
+url = "https://mastodon.social/users/pbandj9819/followers?page=1"
 response = requests.get(url, headers={ "accept": "application/activity+json"})
+print(response.headers)
+print(type(response.headers))
 print(response.text)
