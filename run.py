a47291b49d64fcf6363961cb8f6ce614e8cffae5
Simon Lay <ylay3633@uni.sydney.edu.au>
2024-08-16 17:35:30 +1000

    update webfinger

--- run.py
+++ run.py
@@ -6,8 +6,7 @@ domain = "mastodon.social/users/pbandj9819"
 domain = "mastodon.social/inbox"
 domain = "gopinath.org/actors/rahul"
 url = "https://mastodon.social/users/pbandj9819/following?page=1"
-url = "https://dlay.freemyip.com"
+# url = 'https://rahul.gopinath.org/.well-known/webfinger?resource=acct:rahul@rahul.gopinath.org'
 response = requests.get(url, headers={ "accept": "application/activity+json"})
 print(response.headers)
-print(type(response.headers))
 print(response.text)
